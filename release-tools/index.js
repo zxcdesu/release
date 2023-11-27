135f13fdb65ecd25ebb95760f823e696fee1856c
xanedesu <39487012+xanedesu@users.noreply.github.com>
2023-11-27 09:50:34 +0000

    3

--- release-tools/index.js
+++ release-tools/index.js
@@ -1,2 +1,4 @@
+const {execSync} = require('child_process');
+
 const version = process.argv[2];
-console.log(version);
+console.log(execSync(`git tag --sort=-creatordate | grep -A 1 ${version} | tail -n 1`));
