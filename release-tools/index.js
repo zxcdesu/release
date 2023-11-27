7975b19ec1e139e89d9ece31e5d41be477a15518
xanedesu <39487012+xanedesu@users.noreply.github.com>
2023-11-27 10:03:03 +0000

    3

--- release-tools/index.js
+++ release-tools/index.js
@@ -1,10 +1,5 @@
 const {execSync} = require('child_process');
 
-const version = process.argv[2];
-console.log(
-  'current',
-  version,
-  'previous',
-  execSync(`git describe --abbrev=0 --tags`, { encoding: 'utf8' }),
-  execSync(`git describe --abbrev=0 --tags ${version}^`, { encoding: 'utf8' }),
-);
+console.log(1, version);
+console.log(2, execSync(`git describe --abbrev=0 --tags`, { encoding: 'utf8' }));
+console.log(3, execSync(`git describe --abbrev=0 --tags ${version}^`, { encoding: 'utf8' }));
