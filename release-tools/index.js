c13ded914c7b83b83a5229b2ee5c8e6f091799e3
xanedesu <39487012+xanedesu@users.noreply.github.com>
2023-11-30 07:57:47 +0000

    build

--- release-tools/index.js
+++ release-tools/index.js
@@ -1,9 +1,65 @@
 const {execSync} = require("child_process");
-const {appendFileSync} = require("fs");
+const {
+  appendFileSync,
+  existsSync,
+  readdirSync: readDirSync,
+  readFileSync,
+  writeFileSync,
+} = require("fs");
 const {EOL} = require("os");
+const {join} = require("path");
 
 function setOutput(key, value) {
   appendFileSync(process.env["GITHUB_OUTPUT"], `${key}=${value}`.concat(EOL));
+}
+
+function getWorkspaces(root) {
+  return readDirSync(root).map((workspace) => {
+    const packageJsonPath = join(root, workspace, "package.json");
+    const packageJson = JSON.parse(readFileSync(packageJsonPath));
+    return {
+      name: packageJson.name,
+      root,
+      workspace,
+      lib: root === "libs" || existsSync(join(root, workspace, "lib")),
+      app: root === "apps",
+      patch: (name, version) => {
+        let result =
+          name === packageJson.name && packageJson.version !== version;
+
+        if (
+          typeof packageJson.dependencies?.[name] !== "undefined" &&
+          packageJson.dependencies[name] !== version
+        ) {
+          packageJson.dependencies[name] = version;
+          result = true;
+        }
+
+        if (result) {
+          if (packageJson.version !== version) {
+            packageJson.version = version;
+          }
+
+          writeFileSync(
+            packageJsonPath,
+            JSON.stringify(packageJson, null, 2).concat("\n")
+          );
+        }
+
+        return result;
+      },
+    };
+  });
+}
+
+function patchRecursive(workspaces, libs, apps, name, version) {
+  workspaces.forEach((workspace) => {
+    if (workspace.patch(name, version)) {
+      patchRecursive(workspaces, libs, apps, workspace.name, version);
+      if (workspace.lib) libs.add(join(workspace.root, workspace.workspace));
+      if (workspace.app) apps.add(workspace.workspace);
+    }
+  });
 }
 
 const version = process.argv[2];
@@ -14,4 +70,44 @@ const diff = execSync(
   .trim()
   .split("\n");
 
-setOutput("DOCKER", JSON.stringify(diff));
+const apps = new Set();
+const libs = new Set();
+
+for (const path of diff) {
+  const [root, workspace] = path.split("/");
+  if (path.startsWith("libs/") || path.startsWith(`apps/${workspace}/lib/`))
+    libs.add(join(root, workspace));
+  if (path.startsWith("apps/")) apps.add(workspace);
+}
+
+const workspaces = [].concat(getWorkspaces('apps'), getWorkspaces('libs'))
+
+libs.forEach((workspace1) => {
+  patchRecursive(
+    workspaces,
+    libs,
+    apps,
+    workspaces.find(
+      (workspace2) =>
+        join(workspace2.root, workspace2.workspace) === workspace1
+    ).name,
+    version,
+  );
+});
+
+if (libs.size + apps.size > 0) {
+  execSync("git add -A");
+  execSync(`git commit -m "v${version}"`);
+  execSync('git push origin')
+
+  await Promise.all(
+    Array.from(libs).map((workspace1) => {
+      const lib = workspaces.find(
+        (workspace2) => join(workspace2.root, workspace2.workspace) === workspace1
+      ).name;
+      return exec(`yarn workspace ${lib} run lib:build`);
+    })
+  );
+
+  if (apps.size) setOutput("DOCKER", JSON.stringify(apps));
+} else console.log("no changes found");
