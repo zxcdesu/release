0db5abc5acd4d18c63bfc28a6d166b2a61ca451e
xanedesu <39487012+xanedesu@users.noreply.github.com>
2023-11-30 07:39:38 +0000

    6

--- release-tools/index.js
+++ release-tools/index.js
@@ -3,18 +3,15 @@ const {appendFileSync} = require("fs");
 const {EOL} = require("os");
 
 function setOutput(key, value) {
-  // Temporary hack until core actions library catches up with github new recommendations
-  appendFileSync(process.env["GITHUB_OUTPUT"], `${key}=${value}${EOL}`);
+  appendFileSync(process.env["GITHUB_OUTPUT"], `${key}=${value}`.concat(EOL));
 }
 
 const version = process.argv[2];
 const diff = execSync(
   `git --no-pager diff $(git describe --abbrev=0 --tags ${version}^) --minimal --name-only`,
-  {
-    encoding: "utf8",
-  },
 )
+  .toString('utf8')
   .trim()
   .split("\n");
 
-setOutput("release", diff);
+setOutput("DOCKER", JSON.stringify(diff));
